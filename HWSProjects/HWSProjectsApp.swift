172b81bff46e1925fb8e01fe63951d39c1e4d67c
Emile Wong <emilewong@outlook.com>
2021-05-23 15:30:33 +0800

    Initial Commit

---
+++ HWSProjects/HWSProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSProjectsApp.swift
+//  HWSProjects
+//
+//  Created by Emile Wong on 23/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct HWSProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
